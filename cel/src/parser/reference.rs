e6e73f2ebb977992a22ef87e9de5ea66977eeba9
Alex Snaps <alex@wcgw.dev>
2025-07-15 06:43:31 -0400

    feat(types): Slowly wiring types in
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- cel/src/parser/reference.rs
+++ cel/src/parser/reference.rs
@@ -1,3 +1,5 @@
+use crate::common::types;
+use crate::common::types::Type;
 use std::time::{Duration, SystemTime};
 
 #[derive(Clone, Debug, PartialEq)]
@@ -14,11 +16,33 @@ pub enum Val {
     List,
     Map,
     Null,
-    Opaque,
     String(String),
-    Object,
     Timestamp(SystemTime),
     Type,
     UInt(u64),
     Unknown,
+}
+
+impl Val {
+    pub fn get_type(&self) -> Type {
+        match self {
+            Val::Unspecified => types::UNKNOWN_TYPE,
+            Val::Error => types::ERROR_TYPE,
+            Val::Dyn => types::DYN_TYPE,
+            Val::Any => types::ANY_TYPE,
+            Val::Boolean(_) => types::BOOL_TYPE,
+            Val::Bytes(_) => types::BYTES_TYPE,
+            Val::Double(_) => types::DOUBLE_TYPE,
+            Val::Duration(_) => types::DURATION_TYPE,
+            Val::Int(_) => types::INT_TYPE,
+            Val::List => types::LIST_TYPE,
+            Val::Map => types::MAP_TYPE,
+            Val::Null => types::NULL_TYPE,
+            Val::String(_) => types::STRING_TYPE,
+            Val::Timestamp(_) => types::TIMESTAMP_TYPE,
+            Val::Type => types::TYPE_TYPE,
+            Val::UInt(_) => types::UINT_TYPE,
+            Val::Unknown => types::UNKNOWN_TYPE,
+        }
+    }
 }
