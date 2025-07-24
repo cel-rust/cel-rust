991d3b2d272a8be0a31097e6d17d15778db298d4
Clark McCauley <clarkmccauley@gmail.com>
2025-07-24 07:43:15 -0600

    Removed non-`Send`+`Sync` errors from `ParseError`

--- antlr/src/parser.rs
+++ antlr/src/parser.rs
@@ -32,6 +32,7 @@ use std::fmt::Display;
 use std::mem;
 use std::ops::Deref;
 use std::rc::Rc;
+use std::sync::Arc;
 
 pub struct MacroExprHelper<'a> {
     helper: &'a mut ParserHelper,
@@ -70,11 +71,11 @@ impl Error for ParseErrors {}
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct ParseError {
-    pub source: Option<Box<dyn Error>>,
+    pub source: Option<String>,
     pub pos: (isize, isize),
     pub msg: String,
     pub expr_id: u64,
-    pub source_info: Option<Rc<SourceInfo>>,
+    pub source_info: Option<Arc<SourceInfo>>,
 }
 
 impl Display for ParseError {
@@ -198,7 +199,7 @@ impl Parser {
         let r = match prsr.start() {
             Ok(t) => Ok(self.visit(t.deref())),
             Err(e) => Err(ParseError {
-                source: Some(Box::new(e)),
+                source: Some(e.to_string()),
                 pos: (0, 0),
                 msg: "UNKNOWN".to_string(),
                 expr_id: 0,
@@ -207,7 +208,7 @@ impl Parser {
         };
 
         let info = self.helper.source_info;
-        let source_info = Rc::new(info);
+        let source_info = Arc::new(info);
 
         let mut errors = parse_errors.take();
         errors.extend(self.errors);
@@ -331,7 +332,7 @@ impl Parser {
         s: S,
     ) -> IdedExpr {
         let error = ParseError {
-            source: e.map(|e| e.into()),
+            source: e.map(|e| e.to_string()),
             pos: (token.line, token.column + 1),
             msg: s.into(),
             expr_id: 0,
