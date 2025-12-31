7b56cfc9014c57877c1fa1cf290856a6220a093f
Alex Snaps <alex@wcgw.dev>
2025-12-31 06:19:27 -0500

    feat(dyn Val): Duration impl (Adder|Comparer|Substrator)
    Signed-off-by: Alex Snaps <alex@wcgw.dev>

--- cel/src/common/types/duration.rs
+++ cel/src/common/types/duration.rs
@@ -1,5 +1,8 @@
+use crate::common::traits::{Adder, Comparer, Subtractor};
 use crate::common::types::Type;
 use crate::common::value::Val;
+use crate::{ExecutionError, Value};
+use std::borrow::Cow;
 use std::ops::Deref;
 use std::time::Duration as StdDuration;
 
@@ -29,14 +32,74 @@ impl Val for Duration {
         super::DURATION_TYPE
     }
 
-    fn clone_as_boxed(&self) -> Box<dyn Val> {
-        Box::new(Duration(self.0))
+    fn as_adder(&self) -> Option<&dyn Adder> {
+        Some(self)
+    }
+
+    fn as_comparer(&self) -> Option<&dyn Comparer> {
+        Some(self)
+    }
+
+    fn as_subtractor(&self) -> Option<&dyn Subtractor> {
+        Some(self)
     }
 
     fn equals(&self, other: &dyn Val) -> bool {
         other
             .downcast_ref::<Self>()
             .is_some_and(|other| self.0 == other.0)
+    }
+
+    fn clone_as_boxed(&self) -> Box<dyn Val> {
+        Box::new(Duration(self.0))
+    }
+}
+
+impl Adder for Duration {
+    fn add<'a>(&'a self, rhs: &dyn Val) -> Result<Cow<'a, dyn Val>, crate::ExecutionError> {
+        if let Some(rhs) = rhs.downcast_ref::<Duration>() {
+            Ok(Cow::<dyn Val>::Owned(Box::new(Duration(
+                // todo report the proper values in the error
+                self.0.checked_add(rhs.0).ok_or(ExecutionError::Overflow(
+                    "add",
+                    Value::Null,
+                    Value::Null,
+                ))?,
+            ))))
+        } else {
+            Err(crate::ExecutionError::UnsupportedBinaryOperator(
+                "add",
+                (self as &dyn Val).try_into().unwrap_or(Value::Null),
+                rhs.try_into().unwrap_or(Value::Null),
+            ))
+        }
+    }
+}
+
+impl Comparer for Duration {
+    fn compare(&self, rhs: &dyn Val) -> Result<std::cmp::Ordering, ExecutionError> {
+        if let Some(rhs) = rhs.downcast_ref::<Duration>() {
+            Ok(self.0.cmp(&rhs.0))
+        } else {
+            Err(ExecutionError::NoSuchOverload)
+        }
+    }
+}
+
+impl Subtractor for Duration {
+    fn sub<'a>(&'a self, rhs: &'_ dyn Val) -> Result<Cow<'a, dyn Val>, ExecutionError> {
+        if let Some(rhs) = rhs.downcast_ref::<Duration>() {
+            Ok(Cow::<dyn Val>::Owned(Box::new(Duration(
+                // todo report the proper values in the error
+                self.0.checked_sub(rhs.0).ok_or(ExecutionError::Overflow(
+                    "add",
+                    Value::Null,
+                    Value::Null,
+                ))?,
+            ))))
+        } else {
+            Err(ExecutionError::NoSuchOverload)
+        }
     }
 }
 
